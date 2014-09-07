aa6e981116e98818d4323f7189dc758c44474d2f
Andy <thraka.accounts@outlook.com>
2014-09-07 12:53:25 -0700

    Temp push with placeholder classes

---
+++ Editors/DrawingEditor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SadConsoleEditor.Editors
+{
+    class DrawingEditor
+    {
+
+    }
+}
