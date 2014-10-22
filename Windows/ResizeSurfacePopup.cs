0c0ac5255986364dc302f9406d4ff1f8f4a98ef4
Andy <thraka.accounts@outlook.com>
2014-10-22 00:04:18 -0700

    Added missing file

---
+++ Windows/ResizeSurfacePopup.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Xna.Framework;
+using SadConsole.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SadConsoleEditor.Windows
+{
+    public class ResizeSurfacePopup: SadConsole.Consoles.Window
+    {
+        #region Fields
+        private Button _okButton;
+        private Button _cancelButton;
+
+        private InputBox _widthBox;
+        private InputBox _heightBox;
+        //private InputBox _name;
+        #endregion
+
+        #region Properties
+        public int SettingHeight { get; private set; }
+        public int SettingWidth { get; private set; }
+        #endregion
+
+        public ResizeSurfacePopup(int width, int height)
+            : base(22, 7)
+        {
+            //this.DefaultShowPosition = StartupPosition.CenterScreen;
+            Title = "Resize";
+
+            _cellData.DefaultBackground = Settings.Color_MenuBack;
+            _cellData.DefaultForeground = Settings.Color_TitleText;
+
+            _okButton = new Button(8, 1)
+            {
+                Text = "Accept",
+                Position = new Microsoft.Xna.Framework.Point(base.CellData.Width - 10, 5)
+            };
+            _okButton.ButtonClicked += new EventHandler(_okButton_Action);
+
+            _cancelButton = new Button(8, 1)
+            {
+                Text = "Cancel",
+                Position = new Microsoft.Xna.Framework.Point(2, 5)
+            };
+            _cancelButton.ButtonClicked += new EventHandler(_cancelButton_Action);
+
+            //Print(2, 3, "Name");
+            CellData.Print(2, 2, "Width");
+            CellData.Print(2, 3, "Height");
+
+            _widthBox = new InputBox(3)
+            {
+                Text = width.ToString(),
+                MaxLength = 3,
+                IsNumeric = true,
+                Position = new Microsoft.Xna.Framework.Point(base.CellData.Width - 5, 2)
+            };
+
+            _heightBox = new InputBox(3)
+            {
+                Text = height.ToString(),
+                MaxLength = 3,
+                IsNumeric = true,
+                Position = new Microsoft.Xna.Framework.Point(base.CellData.Width - 5, 3)
+            };
+
+            //_name = new InputBox(20)
+            //{
+            //    Text = name,
+            //    Position = new Microsoft.Xna.Framework.Point(9, 3)
+            //};
+
+            Add(_widthBox);
+            Add(_heightBox);
+            Add(_cancelButton);
+            Add(_okButton);
+            //Add(_name);
+        }
+
+        void _cancelButton_Action(object sender, EventArgs e)
+        {
+            DialogResult = false;
+            Hide();
+        }
+
+        void _okButton_Action(object sender, EventArgs e)
+        {
+            DialogResult = true;
+
+            int width = int.Parse(_widthBox.Text);
+            int height = int.Parse(_heightBox.Text);
+
+            SettingWidth = width < 1 ? 1 : width;
+            SettingHeight = height < 1 ? 1 : height;
+            //Name = _name.Text;
+
+            Hide();
+        }
+    }
+}
---
+++ Windows/SelectFilePopup.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SadConsole;
+using Microsoft.Xna.Framework;
+using SadConsole.Controls;
+using SadConsole.Consoles;
+namespace SadConsoleEditor.Windows
+{
+    class SelectFilePopup : Window
+    {
+        #region Fields
+        private string _currentFolder;
+        private SadEditor.Controls.FileDirectoryListbox _directoryListBox;
+        private InputBox _fileName;
+        private Button _selectButton;
+        private Button _cancelButton;
+        #endregion
+
+        #region Properties
+        public string CurrentFolder
+        {
+            get { return _directoryListBox.CurrentFolder; }
+            set { _directoryListBox.CurrentFolder = value; }
+        }
+
+        public string FileFilter
+        {
+            get { return _directoryListBox.FileFilter; }
+            set { _directoryListBox.FileFilter = value; }
+        }
+
+        public string PreferredExtensions
+        {
+            get { return _directoryListBox.HighlightedExtentions; }
+            set { _directoryListBox.HighlightedExtentions = value; }
+        }
+
+        public string SelectedFile { get; private set; }
+        #endregion
+
+        #region Constructors
+
+        public SelectFilePopup()
+            : base(50, 30)
+        {
+            Title = "Select File";
+            _directoryListBox = new SadEditor.Controls.FileDirectoryListbox()
+            {
+                Position = new Point(1, 1)
+            };
+            _directoryListBox.HighlightedExtentions = ".pdb;.xml";
+            _directoryListBox.Resize(this.CellData.Width - 2, this.CellData.Height - 5);
+            _directoryListBox.SelectedItemChanged += _directoryListBox_SelectedItemChanged;
+            _directoryListBox.SelectedItemExecuted += _directoryListBox_SelectedItemExecuted;
+
+            _fileName = new InputBox(this.CellData.Width - 11)
+            {
+                Position = new Point(2, this.CellData.Height - 3),
+            };
+
+            _selectButton = new Button(6, 1)
+            {
+                Text = "Open",
+                Position = new Point(this.CellData.Width - 8, this.CellData.Height - 3)
+            };
+            _selectButton.ButtonClicked += new EventHandler(_selectButton_Action);
+
+            _cancelButton = new Button(6, 1)
+            {
+                Text = "Cancel",
+                Position = new Point(this.CellData.Width - 8, this.CellData.Height - 2)
+            };
+            _cancelButton.ButtonClicked += new EventHandler(_cancelButton_Action);
+
+            Add(_directoryListBox);
+            Add((_fileName));
+            Add((_selectButton));
+            Add((_cancelButton));
+        }
+        #endregion
+
+        public override void Show(bool modal)
+        {
+            SelectedFile = "";
+            base.Show(modal);
+        }
+
+        void _cancelButton_Action(object sender, EventArgs e)
+        {
+            DialogResult = false;
+            Hide();
+        }
+
+        void _selectButton_Action(object sender, EventArgs e)
+        {
+            if (_fileName.Text != string.Empty)
+            {
+                SelectedFile = System.IO.Path.Combine(_directoryListBox.CurrentFolder, _fileName.Text);
+                DialogResult = true;
+                Hide();
+            }
+        }
+
+        void _directoryListBox_SelectedItemExecuted(object sender, SadEditor.Controls.FileDirectoryListbox.SelectedItemEventArgs e)
+        {
+
+        }
+
+        void _directoryListBox_SelectedItemChanged(object sender, SadEditor.Controls.FileDirectoryListbox.SelectedItemEventArgs e)
+        {
+            if (e.Item is System.IO.FileInfo)
+                _fileName.Text = ((System.IO.FileInfo)e.Item).Name;
+            else if (e.Item is SadEditor.Controls.HighlightedExtFile)
+                _fileName.Text = ((SadEditor.Controls.HighlightedExtFile)e.Item).Name;
+            else
+                _fileName.Text = "";
+
+            _selectButton.IsEnabled = _fileName.Text != "";
+        }
+    }
+}
