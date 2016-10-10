70a69c2638585f50f1b0150f9c584d7183afc32d
Andy De George <thraka@outlook.com>
2016-10-09 20:06:57 -0700

    Added resize back in!

--- Editors/IEditor.cs
+++ Editors/IEditor.cs
@@ -55,74 +55,9 @@ namespace SadConsoleEditor.Editors
 
         void Save();
 
-        void Reset();
-
-        void OnSelected();
-
-        void OnDeselected();
-
-        void OnClosed();
-    }
-
-    public interface IEditor2
-    {
-        EditorSettings Settings { get; }
-
-        event EventHandler<MouseEventArgs> MouseEnter;
-        event EventHandler<MouseEventArgs> MouseExit;
-        event EventHandler<MouseEventArgs> MouseMove;
-
-        int Width { get; }
-        int Height { get; }
-
-        string Title { get; }
-
-        string ShortName { get; }
-
-        string Id { get; }
-
-        string[] Tools { get; }
-
-        IEnumerable<IFileLoader> FileExtensionsLoad { get; }
-        IEnumerable<IFileLoader> FileExtensionsSave { get; }
-
-        ITextSurface Surface { get; }
-
-        //CustomPanel[] ControlPanels { get; }
-
-        void ProcessKeyboard(KeyboardInfo info);
-
-        void ProcessMouse(MouseInfo info);
-
         void Resize(int width, int height);
 
-        void Position(int x, int y);
-
-        Point GetPosition();
-
-        void Render();
-
-        void Update();
-
-        void Load(string file, IFileLoader loader);
-
-        void Save(string file, IFileLoader loader);
-
         void Reset();
-
-        void RemoveLayer(int index);
-
-        void MoveLayerUp(int index);
-
-        void MoveLayerDown(int index);
-
-        void AddNewLayer(string name);
-
-        bool LoadLayer(string file);
-
-        void SaveLayer(int index, string file);
-
-        void SetActiveLayer(int index);
 
         void OnSelected();
 
